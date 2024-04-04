678ef750a69c07a16f16447405d93115866e8d03
cjmatheka <cjmatheka@gmail.com>
2024-04-04 15:48:57 +0300

    Update Task 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -13,10 +13,10 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left != NULL && tree->right != NULL)
+	if (tree->left == NULL && tree->right == NULL)
 		return (0);
 
-	num = 1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right);
+	num = binary_tree_nodes(tree->left) + 1 + binary_tree_nodes(tree->right);
 
 	return (num);
 }
