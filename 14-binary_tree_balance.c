d7dd3fd83ea966fbe486f90124138e413650c0a1
cjmatheka <cjmatheka@gmail.com>
2024-04-04 16:34:20 +0300

    Add Task 15

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -17,3 +17,25 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	return (balance);
 }
+
+/**
+ * binary_tree_height - measures the height of binary tree
+ * @tree: pointer to the binary tree
+ * Return: size of binary tree
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height, tree_height;
+
+	if (tree == NULL)
+		return (0);
+
+	left_height = 0, right_height = 0;
+
+	left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	tree_height = ((left_height > right_height) ? left_height : right_height);
+
+	return (tree_height);
+}
