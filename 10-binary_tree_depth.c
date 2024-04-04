0689eec7689c293528c4331cd2483e2ff7f8035c
cjmatheka <cjmatheka@gmail.com>
2024-04-04 15:03:57 +0300

    Update Task 10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,16 +9,12 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t left_depth, right_depth, tree_depth;
+	size_t tree_depth;
 
 	if (tree == NULL)
 		return (0);
 
-	left_depth = 0, right_depth = 0;
-
-	left_depth = tree->left ? 1 + binary_tree_depth(tree->left) : 0;
-	left_depth = tree->right ? 1 + binary_tree_depth(tree->right) : 0;
-	tree_depth = ((left_depth > right_depth) ? left_depth : right_depth);
+	tree_depth = ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
 
 	return (tree_depth);
 }
