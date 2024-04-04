53fb6d8dfa4e50d0eead7337cf4ef783cdd997d6
cjmatheka <cjmatheka@gmail.com>
2024-04-04 14:53:01 +0300

    Update Task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,9 +13,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-	tree_height = 1 + (left_height > right_height ? left_height : right_height);
+	left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	tree_height = ((left_height > right_height) ? left_height : right_height);
 
 	return (tree_height);
 }
